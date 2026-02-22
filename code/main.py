5f85ba1166658c17b230bf5fdbd56694ff3e7409
Rares Igescu <raresigescu@gmail.com>
2026-02-22 17:58:32 +0200

    Add the data loading and preprocessing code

---
+++ code/main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import gymnasium as gym
+from gymnasium import spaces
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
+import time
+
+# === LOAD AND PREPROCESS DATA ===
+# In order for the AI agent to actually learn different attack types,
+# we need to preprocess the traffic data and convert it into a numerical format.
+
+# Load the CICIDS2017 dataset 
+data = pd.read_csv('code/data/cicids2017_cleaned.csv')
+
+# Remove any hidden spaces from column names
+data.columns = data.columns.str.strip()
+
+# Replace the infinite values with NaN and then drop any rows that contain NaN values to ensure that our dataset is clean and ready for training
+data.replace([np.inf, -np.inf], np.nan, inplace=True)
+data.dropna(inplace=True)
+
+# Encode the labels in a binary format: 0 for normal traffic and 1 for any kind of attack 
+answer = data['Attack Type'].apply(lambda x: 0 if x == 'Normal Traffic' else 1) 
+
+# Drop the last comun named 'Attack Type'
+inputData = data.drop('Attack Type', axis=1)
+
+# We need to scale the input data to ensure that all features are on the same scale, which can help the AI agent learn more effectively.
+scaler = MinMaxScaler()
+inputData_scaled = scaler.fit_transform(inputData)
+
+# Print the distribution of attack types and the range of the scaled input data to verify that the preprocessing steps have been applied correctly.
+# The first print will show a list of attack types and their corresponding counts in the dataset
+# The second print will show the minimum and maximum values of the scaled input data, which should be between 0 and 1 due to the use of MinMaxScaler.
+print(data['Attack Type'].value_counts())
+print(inputData_scaled.min(), inputData_scaled.max())
+
+# === Define the environment ===
+
+# === Define the agent ===
+
+# === Evaluation === 
